f127f8b254bfbb56ea17659811685fc49585a9e2
florianmathieudev <florianmathieu.dev@gmail.com>
2019-10-15 15:26:06 +0200

    js en cours

---
+++ app.js
@@ -0,0 +1,66 @@
+const ratio = .3
+const ratioHR = .1
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        } 
+    })
+}
+  
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
+})
+
+
+
+const handleIntersectH = function (entries, observerH) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible-H')
+            observerH.unobserve(entry.target)
+        } 
+    })
+}
+  
+const observerH = new IntersectionObserver(handleIntersectH, options);
+document.querySelectorAll('.reveal-H').forEach(function (rh) {
+    observerH.observe(rh)
+})
+
+const handleIntersectHR = function (entries, observerHR) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratioHR) {
+            entry.target.classList.add('reveal-visible-H-R')
+            observerHR.unobserve(entry.target)
+        } 
+    })
+}
+  
+const observerHR = new IntersectionObserver(handleIntersectHR, options);
+document.querySelectorAll('.reveal-H-R').forEach(function (rhr) {
+    observerHR.observe(rhr)
+})
+
+
+const handleIntersectV = function (entries, observerV) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('revealV-visible')
+            observerV.unobserve(entry.target)
+        } 
+    })
+}
+  
+const observerV = new IntersectionObserver(handleIntersectV, options);
+document.querySelectorAll('[class*="revealV-"]').forEach(function (rV) {
+    observerV.observe(rV)
+})
